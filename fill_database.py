1e69aa8b3940eeb8cb62509e5efb9496c875bebb
Hussan K <hussankhanwork@gmail.com>
2018-11-05 19:08:14 -0600

    minor errors

--- fill_database.py
+++ fill_database.py
@@ -30,8 +30,8 @@ import os
 import time
 from twilio.rest import Client
 
-account = "ACe2e01506eb05a0e4b01dd8e4a4244fa5"
-token = "a0b6e07d6f8e06c3516261262d41bc70"
+account = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 client = Client(account, token)
 
 message = client.messages.create(to="+12242412335", from_="+12242368932",
